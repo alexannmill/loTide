2ba8b1c3222b9660376ce2d9e2d0a2908ed1e802
alexannmill <alexannmill@gmail.com>
2022-08-23 18:47:26 +0000

    tail changed to slice, pigLatian added

---
+++ pig.latain.js
@@ -0,0 +1,11 @@
+function pigLatian(string) {
+  let firstL = string[0];
+  // piggy = piggy.shift()
+ let piggy = string.slice(1)
+  piggy = piggy + firstL;
+  piggy = piggy + 'ay';
+
+  return piggy;
+}
+
+console.log(pigLatian('hello'));
--- tail.js
+++ tail.js
@@ -1,12 +1,13 @@
 const tail = function(actual) {
-  let array = actual.splice(1);
+  let array = actual.slice(1);
   return array;
 };
 
-
+console.log(tail('hello'))
 // const assertEqual = function(actual, expected) {
 //   if (actual === expected) console.log(`✅  Assertion Pass ${actual} === ${expected}`);
 //   else console.log(`⛔  Assertion Fail  ${actual} !== ${expected}`);
 // };
 // const result = tail(["Hello", "Lighthouse", "Labs"]);
 // assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
