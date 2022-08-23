323a04824d2e750e8e75ada73c6110b533f91e04
alexannmill <alexannmill@gmail.com>
2022-08-23 02:56:35 +0000

    emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) console.log("Assertion Pass " + actual + "===" + expected);
-    else console.log("Assertion Fail " + actual + "!==" + expected);
+  if (actual === expected) console.log(`✅  Assertion Pass ${actual} === ${expected}`);
+    else console.log(`⛔  Assertion Fail  ${actual} !== ${expected}`)
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
