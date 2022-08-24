09dc39de7bf2456d70bca8a6e117ffaf3c7f88af
alexannmill <alexannmill@gmail.com>
2022-08-24 05:32:55 +0000

    editing testing code for proper run

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,18 +4,28 @@
 // };
 function eqArrays(ar1, ar2){
   if ( ar1.length !== ar2.length){ 
-    return `⛔ ${ar1} === ${ar2}`;
+    return false;
   }
   for (let i = 0; i < ar1.length; i++){
       // console.log(ar1[i], ar2[i])
       if(ar1[i] !== ar2[i]){
-        return `⛔ ${ar1} !== ${ar2}`;
+        return false;
       }
   }
-  return `✅ ${ar1} === ${ar2}`;
+  return true;
+}
+function assertEqualArray(ar1, ar2){
+  if(eqArrays(ar1, ar2)){
+    console.log(`✅  Assertion Pass ${ar1} === ${ar2}`)
+  }
+  else {
+     console.log(`⛔  Assertion Fail  ${ar1} !== ${ar2}`)
+  }
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]), true);
-console.log(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqualArray([1, 2, 3], [1, 2, 3]);
+
+eqArrays([1, 2, 3], [1, 2, 3]), true;
+eqArrays([1, 2, 3], [3, 2, 1]), false;
 eqArrays(["1", "2", "3"], ["1", "2", "3"]), true;
 eqArrays(["1", "2", "3"], ["1", "2", 3]), false;
