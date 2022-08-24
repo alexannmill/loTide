0e466d0a40be94e77d6ae595a225e09e4c19f4d5
alexannmill <alexannmill@gmail.com>
2022-08-24 05:13:21 +0000

    edit return values to proper statment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,19 +4,18 @@
 // };
 function eqArrays(ar1, ar2){
   if ( ar1.length !== ar2.length){ 
-    return false;
+    return `⛔ ${ar1} === ${ar2}`;
   }
   for (let i = 0; i < ar1.length; i++){
       // console.log(ar1[i], ar2[i])
       if(ar1[i] !== ar2[i]){
-        return false;
+        return `⛔ ${ar1} !== ${ar2}`;
       }
   }
-  return true;
+  return `✅ ${ar1} === ${ar2}`;
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]), true);
-eqArrays([1, 2, 3], [3, 2, 1]), false;
+console.log(eqArrays([1, 2, 3], [3, 2, 1]), false);
 eqArrays(["1", "2", "3"], ["1", "2", "3"]), true;
 eqArrays(["1", "2", "3"], ["1", "2", 3]), false;
