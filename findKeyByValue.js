6624e10d17b4ea258393751d4953807c6e8e2688
alexannmill <alexannmill@gmail.com>
2022-08-25 02:36:48 +0000

    completed find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+//Test Code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion Pass ${actual} === ${expected}`);
+  };
+  if (actual !== expected) {
+    console.log(`⛔  Assertion Fail  ${actual} !== ${expected}`);
+  };
+};
+
+//Actual Function
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+function findKeyByValue(object, value) {
+  for (key of Object.keys(object)) {
+    if (value === object[key]) {
+      return key;
+    };
+  };
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "sci_fi"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
