561af9995c2299d709ed591fca18605e98bbe925
alexannmill <alexannmill@gmail.com>
2022-08-25 04:15:26 +0000

    removed pues and reformatted

--- eqObjects.js
+++ eqObjects.js
@@ -8,31 +8,31 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  
-  if (Object.keys(object1).length !== Object.keys(object2).length){
-    return false
+
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
   }
-  for (key in object1){
-    if(object1[key] !== object2[key]){
-      return false
-    }
-    if (Array.isArray(object1[key]) !== Array.isArray(object1[key])){
-      return false
-    }
-    if ( object1[key].length !== object2[key].length){ 
+  for (key in object1) {
+    if (object1[key] !== object2[key]) {
       return false;
     }
-    for (let i = 0; i < object1[key].length; i++){
-        // console.log(object1[key][i], obn[i])
-        if(object1[key][i] !== object2[key][i]){
-          return false;
-        }
+    if (Array.isArray(object1[key]) !== Array.isArray(object1[key])) {
+      return false;
+    }
+    if (object1[key].length !== object2[key].length) {
+      return false;
+    }
+    for (let i = 0; i < object1[key].length; i++) {
+      // console.log(object1[key][i], obn[i])
+      if (object1[key][i] !== object2[key][i]) {
+        return false;
+      }
     }
     return true;
-    };
-    return true
+  };
+  return true;
 };
-    
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(eqObjects(ab, ba)); // => true
