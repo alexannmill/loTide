d538bebb8f469dd346ab54a0a84e01e5a011081e
alexannmill <alexannmill@gmail.com>
2022-08-25 19:28:44 +0000

    cleaning up eqO and completing assertOE file

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,39 +1,32 @@
-const eqObjects = function(object1, object2) {
 
+const eqObjects = function(object1, object2) {
+  
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-   console.log(Object.keys(object1).length)
     return false;
   }
-  for (key in object1) {
-    if (object1[key] !== object2[key]) {
-      console.log(object1[key])
-      return false;
-    }
-    if (Array.isArray(object1[key]) !== Array.isArray(object1[key])) {
-      return false;
-    }
-    if (object1[key].length !== object2[key].length) {
-      return false;
-    }
-    for (let i = 0; i < object1[key].length; i++) {
-      // console.log(object1[key][i], obn[i])
-      if (object1[key][i] !== object2[key][i]) {
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) ){
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
-    return true;
+ 
   };
   return true;
 };
 
+
 const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect; // <= add this line
   if(eqObjects(object1, object2)){
-    console.log(`✅  Assertion Pass ${object1} === ${object2}`)
+    console.log(`✅  Assertion Pass ${inspect(object1)} === ${inspect(object2)}`)
   }
   else {
-    console.log(`⛔  Assertion Fail  ${object1} !== ${object2}`)
+    console.log(`⛔  Assertion Fail  ${inspect(object1)} !== ${inspect(object2)}`)
   }
 }
-const inspect = require('util').inspect; // <= add this line
-console.log(`Example label: ${inspect(actual)}`);
 
+assertObjectsEqual({a:1},{a:1});
+assertObjectsEqual({a:1},{a:2});
--- eqObjects.js
+++ eqObjects.js
@@ -7,37 +7,32 @@ const assertEqual = function(actual, expected) {
   };
 };
 
-function eqArrays(ar1, ar2){
-  if ( ar1.length !== ar2.length){ 
+function eqArrays(ar1, ar2) {
+  if (ar1.length !== ar2.length) {
     return false;
   }
-  for (let i = 0; i < ar1.length; i++){
-      // console.log(ar1[i], ar2[i])
-      if(ar1[i] !== ar2[i]){
-        return false;
-      }
+  for (let i = 0; i < ar1.length; i++) {
+    // console.log(ar1[i], ar2[i])
+    if (ar1[i] !== ar2[i]) {
+      return false;
+    }
   }
   return true;
 }
 
 const eqObjects = function(object1, object2) {
-  
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key in object1) {
-    if (Array.isArray(object1[key]) ){
+    if (Array.isArray(object1[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else if (object1[key] !== object2[key]) {
       return false;
     }
-    // for (let i = 0; i < object1[key].length; i++) {
-    //   if (object1[key][i] !== object2[key][i]) {
-    //     return false;
-    //   }
-    // return true;
   };
   return true;
 };
@@ -45,15 +40,15 @@ const eqObjects = function(object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba),true); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc),false); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 
-assertEqual(eqObjects(cd, dc),true); // => true
+assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2),false); // => false
+assertEqual(eqObjects(cd, cd2), false); // => false
