68f4716bba0fb0dfc7f97e1ebd8cea8ca0ad5fef
alexannmill <alexannmill@gmail.com>
2022-08-29 22:15:50 +0000

    adding .json files along -- adding gitignore -- updated head func with mocha/chai

--- test/headTest.test.js
+++ test/headTest.test.js
@@ -1,6 +1,14 @@
-const head = require("../head")
-const assertEqual = require("../assertEqual")
+const head = require("../head");
+const assert = require("chai").assert;
 
-
-assertEqual(head("hello"), "h");
-assertEqual(head("alex"), "a") 
+describe("testing the head function", () => {
+  it("should return undefined if empty sting", () => {
+    assert.strictEqual(head(""), undefined);
+  });
+  it("returns the first letter in a string", () => {
+    assert.strictEqual(head("hello"), "h");
+  });
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+});
